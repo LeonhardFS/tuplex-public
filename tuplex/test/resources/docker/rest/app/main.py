73310349baaea83a8418c3bf380a89f202a41439
Leonhard Spiegelberg <leonhard@brown.edu>
2024-09-10 23:53:09 -0700

    more routes

--- tuplex/test/resources/docker/rest/app/main.py
+++ tuplex/test/resources/docker/rest/app/main.py
@@ -3,6 +3,7 @@
 import asyncio
 import json
 import logging
+from tkinter.constants import CURRENT
 from typing import Union, List, Dict, Any
 
 import sysconfig
@@ -17,10 +18,15 @@ logger = logging.getLogger(__name__)
 
 # Global variables.
 CURRENT_CONCURRENCY=1
+CURRENT_MEMORY_SIZE=3000
+CURRENT_TIMEOUT=1000
 
 # src: https://docs.aws.amazon.com/lambda/latest/api/API_ListFunctions.html
 @app.route("/2015-03-31/functions/")
 def list_functions(FunctionVersion:str='ALL', Marker:str=None, MasterRegion:str=None, MaxItems:int=50):
+
+    global CURRENT_MEMORY_SIZE
+    global CURRENT_TIMEOUT
 
     # Check under which architecture docker runs,
     # it's either x86_64 or arm64.
@@ -91,7 +97,7 @@ def list_functions(FunctionVersion:str='ALL', Marker:str=None, MasterRegion:str=
                     "SystemLogLevel": "string"
                 },
                 "MasterArn": "string",
-                "MemorySize": 10000,
+                "MemorySize": CURRENT_MEMORY_SIZE,
                 "PackageType": "string",
                 "RevisionId": "string",
                 "Role": "string",
@@ -112,7 +118,7 @@ def list_functions(FunctionVersion:str='ALL', Marker:str=None, MasterRegion:str=
                 "State": "string",
                 "StateReason": "string",
                 "StateReasonCode": "string",
-                "Timeout": 1500,
+                "Timeout": CURRENT_TIMEOUT,
                 "TracingConfig": {
                     "Mode": "string"
                 },
@@ -164,6 +170,118 @@ def put_concurrency(function_name):
     CURRENT_CONCURRENCY = payload["ReservedConcurrentExecutions"]
 
     return concurrency(function_name)
+
+# src: https://docs.aws.amazon.com/lambda/latest/api/API_UpdateFunctionConfiguration.html
+# PUT /2015-03-31/functions/FunctionName/configuration
+@app.route("/2015-03-31/functions/<function_name>/configuration", methods=['PUT'])
+def put_configuration(function_name):
+
+    global CURRENT_MEMORY_SIZE
+    global CURRENT_TIMEOUT
+
+    raw_payload=request.get_data()
+    payload = json.loads(raw_payload.decode())
+
+    if payload.get("MemorySize"):
+        CURRENT_MEMORY_SIZE = payload["MemorySize"]
+    if payload.get("Timeout"):
+        CURRENT_TIMEOUT = payload["Timeout"]
+
+    response = {
+        "Architectures": [ "string" ],
+        "CodeSha256": "string",
+        "CodeSize": 1000,
+        "DeadLetterConfig": {
+            "TargetArn": "string"
+        },
+        "Description": "string",
+        "Environment": {
+            "Error": {
+                "ErrorCode": "string",
+                "Message": "string"
+            },
+            "Variables": {
+                "string" : "string"
+            }
+        },
+        "EphemeralStorage": {
+            "Size": 512
+        },
+        "FileSystemConfigs": [
+            {
+                "Arn": "string",
+                "LocalMountPath": "string"
+            }
+        ],
+        "FunctionArn": "string",
+        "FunctionName": function_name,
+        "Handler": "string",
+        "ImageConfigResponse": {
+            "Error": {
+                "ErrorCode": "string",
+                "Message": "string"
+            },
+            "ImageConfig": {
+                "Command": [ "string" ],
+                "EntryPoint": [ "string" ],
+                "WorkingDirectory": "string"
+            }
+        },
+        "KMSKeyArn": "string",
+        "LastModified": "string",
+        "LastUpdateStatus": "string",
+        "LastUpdateStatusReason": "string",
+        "LastUpdateStatusReasonCode": "string",
+        "Layers": [
+            {
+                "Arn": "string",
+                "CodeSize": 150,
+                "SigningJobArn": "string",
+                "SigningProfileVersionArn": "string"
+            }
+        ],
+        "LoggingConfig": {
+            "ApplicationLogLevel": "string",
+            "LogFormat": "string",
+            "LogGroup": "string",
+            "SystemLogLevel": "string"
+        },
+        "MasterArn": "string",
+        "MemorySize": CURRENT_MEMORY_SIZE,
+        "PackageType": "string",
+        "RevisionId": "string",
+        "Role": "string",
+        "Runtime": "string",
+        "RuntimeVersionConfig": {
+            "Error": {
+                "ErrorCode": "string",
+                "Message": "string"
+            },
+            "RuntimeVersionArn": "string"
+        },
+        "SigningJobArn": "string",
+        "SigningProfileVersionArn": "string",
+        "SnapStart": {
+            "ApplyOn": "string",
+            "OptimizationStatus": "string"
+        },
+        "State": "string",
+        "StateReason": "string",
+        "StateReasonCode": "string",
+        "Timeout": CURRENT_TIMEOUT,
+        "TracingConfig": {
+            "Mode": "string"
+        },
+        "Version": "string",
+        "VpcConfig": {
+            "Ipv6AllowedForDualStack": False,
+            "SecurityGroupIds": [ "string" ],
+            "SubnetIds": [ "string" ],
+            "VpcId": "string"
+        }
+    }
+
+    return jsonify(response)
 
 # src: https://docs.aws.amazon.com/lambda/latest/api/API_Invoke.html
 # POST /2015-03-31/functions/FunctionName/invocations?Qualifier=Qualifier HTTP/1.1
