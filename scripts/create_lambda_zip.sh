eec8eb2ba8df6e72310ea88e75e94fb40eb5d68a
Leonhard Spiegelberg <leonhard@brown.edu>
2024-10-03 18:17:48 -0700

    fixes

--- scripts/create_lambda_zip.sh
+++ scripts/create_lambda_zip.sh
@@ -13,13 +13,15 @@ PYTHON3_VERSION="${PYTHON3_VERSION:-$DEFAULT_PYTHON3_VERSION}"
 PYTHON3_MAJMIN=${PYTHON3_VERSION%.*}
 DOCKER_IMAGE=tuplex/ci:${PYTHON3_MAJMIN}
 
+echo "-- Using image ${DOCKER_IMAGE}"
+
 # check which Python version is installed in /opt/lambda-python/bin/python3
 DOCKER_PYTHON3_VERSION=$(docker run -e LD_LIBRARY_PATH=/opt/lambda-python/lib $DOCKER_IMAGE /opt/lambda-python/bin/python3 --version | cut -d ' ' -f2)
 
 echo "-- detected docker Python3 version ${DOCKER_PYTHON3_VERSION}"
 
 ## make sure maj.min version matches
-if [ "${DOCKER_PYTHON3_VERSION%.*}" -ne "${PYTHON3_VERSION%.*}" ]; then
+if [[ "${DOCKER_PYTHON3_VERSION%.*}" -ne "${PYTHON3_VERSION%.*}" ]]; then
   echo "ERROR: Python maj.min versions do not match, Docker has ${DOCKER_PYTHON3_VERSION%.*} but desired version is ${PYTHON3_VERSION%.*}."
   exit 1
 fi
@@ -81,7 +83,7 @@ echo ">>> Passing following additional args to zip_cc_runtime.py: ${ADD_ZIP_ARGS
 # adjust based on RAM and cores.
 N_PARALLEL=12
 
-docker run --name build-lambda --rm -v $SRC_FOLDER:/code/tuplex -v $LOCAL_BUILD_FOLDER:/build ${DOCKER_IMAGE} bash -c "export LD_LIBRARY_PATH=/opt/lambda-python/lib:/opt/lib:/opt/lib64:\$LD_LIBRARY_PATH && /opt/lambda-python/bin/python${PYTHON3_MAJMIN} -m pip install cloudpickle numpy && cd /build && cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DLLVM_ROOT_DIR=${LLVM_ROOT_PATH} -DBUILD_WITH_CEREAL=${BUILD_WITH_CEREAL} -DBUILD_FOR_LAMBDA=ON -DBUILD_WITH_AWS=ON -DBUILD_WITH_ORC=ON -DPYTHON3_EXECUTABLE=/opt/lambda-python/bin/python${PYTHON3_MAJMIN} -DBOOST_ROOT=/opt/boost/python${PYTHON3_MAJMIN}/ -GNinja /code/tuplex && cmake --build . --target runtime --parallel ${N_PARALLEL} && cmake --build . --target tplxlam --parallel ${N_PARALLEL} && python${PYTHON3_MAJMIN} /code/tuplex/python/zip_cc_runtime.py ${ADD_ZIP_ARGS} --input /build/dist/bin/tplxlam --runtime /build/dist/bin/tuplex_runtime.so --python /opt/lambda-python/bin/python${PYTHON3_MAJMIN} --output /build/tplxlam.zip"
+docker run --name build-lambda --rm -v $SRC_FOLDER:/code/tuplex -v $LOCAL_BUILD_FOLDER:/build ${DOCKER_IMAGE} bash -c "export LD_LIBRARY_PATH=/opt/lambda-python/lib:/opt/lib:/opt/lib64:/usr/local/:\$LD_LIBRARY_PATH && /opt/lambda-python/bin/python${PYTHON3_MAJMIN} -m pip install cloudpickle numpy && cd /build && cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DLLVM_ROOT_DIR=${LLVM_ROOT_PATH} -DBUILD_WITH_CEREAL=${BUILD_WITH_CEREAL} -DBUILD_FOR_LAMBDA=ON -DBUILD_WITH_AWS=ON -DBUILD_WITH_ORC=ON -DPYTHON3_EXECUTABLE=/opt/lambda-python/bin/python${PYTHON3_MAJMIN} -DBOOST_ROOT=/opt/boost/python${PYTHON3_MAJMIN}/ -GNinja /code/tuplex && cmake --build . --target runtime --parallel ${N_PARALLEL} && cmake --build . --target tplxlam --parallel ${N_PARALLEL} && python${PYTHON3_MAJMIN} /code/tuplex/python/zip_cc_runtime.py ${ADD_ZIP_ARGS} --input /build/dist/bin/tplxlam --runtime /build/dist/bin/tuplex_runtime.so --python /opt/lambda-python/bin/python${PYTHON3_MAJMIN} --output /build/tplxlam.zip"
 DOCKER_EXIT_CODE=$?
 if [ "${DOCKER_EXIT_CODE}" -eq "0" ]; then
    echo "-- docker command run, zipped Lambda file can be found in: ${LOCAL_BUILD_FOLDER}/tplxlam.zip"
